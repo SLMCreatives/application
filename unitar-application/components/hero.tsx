d61d9a399a27dc6a71c5210a35981fc8042c241f
SLMCreatives <sulaiman@slmcreatives.com>
2025-06-12 02:41:31 +0800

    first init

---
+++ unitar-application/components/hero.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+export function Hero() {
+  return (
+    <div className="flex flex-col gap-6 items-center ">
+      <div className="flex flex-col gap-2 justify-center items-center">
+        <h2 className="text-3xl font-bold text-center">
+          UNITAR Online Application
+        </h2>
+        <p className="text-sm text-muted-foreground">
+          Submit your application online with ease!
+        </p>
+      </div>
+      <div className="flex flex-row gap-4 items-center">
+        <Button variant={"default"} asChild>
+          <Link href="/sign-up">New Application</Link>
+        </Button>
+        <Button variant={"outline"} asChild>
+          <Link href="/login">Existing Student</Link>
+        </Button>
+      </div>
+    </div>
+  );
+}
